50ac7d9bd93aa7a40c37c7f1207604769eabb744
Benny Zhang <zhang.benny@gmail.com>
2016-01-06 19:46:45 -0800

    Added files to file structure, may be changed

--- client/app/app.js
+++ client/app/app.js
@@ -1,4 +1,8 @@
 var app = angular.module('app', [
+  'rollercost.auth',
+  'rollercost.home',
+  'rollercost.userSubmissions',
+  'rollercost.analyze',
   'ngRoute'
 ]);
 
@@ -34,20 +38,20 @@ app.config(function ($routeProvider, $httpProvider) {
     });
     $httpProvider.interceptors.push('AttachTokens');
 })
-// .factory('AttachTokens', function ($window) {
-//   var attach = {
-//     request: function (object) {
-//       // THE NAME OF THE COOKIE IS COM.ROLLERCOST
-//       var jwt = $window.localStorage.getItem('com.rollercost');
-//       if (jwt) {
-//         object.headers['x-access-token'] = jwt;
-//       }
-//       object.headers['Allow-Control-Allow-Origin'] = '*';
-//       return object;
-//     }
-//   };
-//   return attach;
-// })
+.factory('AttachTokens', function ($window) {
+  var attach = {
+    request: function (object) {
+      // THE NAME OF THE COOKIE IS COM.ROLLERCOST
+      var jwt = $window.localStorage.getItem('com.rollercost');
+      if (jwt) {
+        object.headers['x-access-token'] = jwt;
+      }
+      object.headers['Allow-Control-Allow-Origin'] = '*';
+      return object;
+    }
+  };
+  return attach;
+})
 // .run(function ($rootScope, $location, Auth) {
 //   $rootScope.$on('$routeChangeStart', function (evt, next, current) {
 //     if (next.$$route && next.$$route.authenticate && !Auth.isAuth()) {
