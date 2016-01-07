bc7df219b4fa2e1a3b2298ba1977f7221be64dc5
hlyford11 <honree@EDM-Mac680.local>
2016-01-07 11:35:57 -0800

    Fix so can parse body from post requests

--- server/config/routes.js
+++ server/config/routes.js
@@ -7,15 +7,18 @@ var bodyParser = require('body-parser');
 var request = require('request');
 
 module.exports = function (app, express) {  
-
+  // set up static files and serve up the index page
   app.set('views', path.join(__dirname, "/../../client"));  
   app.set('view engine', 'html');
   app.use(express.static(path.join(__dirname, "/../../client")));
-  app.use(bodyParser.json());
-  app.use(bodyParser.urlencoded({ extended: true })); 
 
+  // set up the various backend routes for the app
+  // auth routes
   app.post('/signup', authController.signup);
+
   app.post('/signin', authController.signin);
+
+  // user form submission routes
 
   // app.use(helpers.errorLogger);
   // app.use(helpers.errorHandler);
