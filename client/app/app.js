10cb53035a92f22178187d9e002061fd79999d82
jotakwak <ykwak323@gmail.com>
2016-01-07 12:31:06 -0800

    Commit before lunch

--- client/app/app.js
+++ client/app/app.js
@@ -1,4 +1,5 @@
 var app = angular.module('app', [
+  'rollercost.userSubmission',
   'ngRoute'
 ]);
 
---
+++ client/app/userSubmission/userSubmission.js
@@ -0,0 +1,24 @@
+var userSubmission = angular.module('rollercost.userSubmission', []);
+
+userSubmission.controller('UserSubmissionController', function($scope, $http){
+  $scope.showCount = 0;
+  $scope.loading = false;
+  $scope.data = {};
+  $scope.addData = function(input, category){
+    //add user input to the data
+    $scope.data[category] = input;
+    //increment the showCount to show the next prompt
+    $scope.showCount++;
+  };
+  $scope.submitData = function(data){
+    $scope.loading = true;
+    $http.post('/api/userSubmission', data).
+      success(function(){
+        //if post request successful, reset the view to the initial one
+        $scope.loading = false;
+        $scope.showCount = 0;
+      });
+  };
+
+
+});
