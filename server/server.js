d196630c16b99c42739a942c0440ba69e3c41e5d
hlyford11 <honree@EDM-Mac680.local>
2016-01-09 12:25:11 -0800

    Small change

--- server/server.js
+++ server/server.js
@@ -7,11 +7,6 @@ var app = express();
 var dbURI = process.env.MONGOLAB_URI || 'mongodb://localhost/rollercost';
 
 // connect to mongo db
-// to view prod mongo db: mongodb://dbuser:dbpass@host:port/dbname
-// ours: MONGOLAB_URI: mongodb://heroku_td25rj4p:8r9p0r48cssu2fgnvtim5o0mk5@ds039195.mongolab.com:39195/heroku_td25rj4p
-// general command: mongo somewhere.mongolayer.com:10011/my_database -u username -p password
-// to view prod db from terminal: mongo ds039195.mongolab.com:39195/heroku_td25rj4p -u heroku_td25rj4p -p 8r9p0r48cssu2fgnvtim5o0mk5
-
 mongoose.connect(dbURI);
 
 console.log('MongoDB listening at...' + dbURI);
@@ -23,6 +18,6 @@ require('./config/routes.js')(app, express);
 // start listening to requests on prod vs port 8000
 var port = process.env.PORT || 8000;
 app.listen(port);
-console.log('Rent app listening on ' + port);
+console.log('Rollercost app listening on ' + port);
 
 module.exports = app;
