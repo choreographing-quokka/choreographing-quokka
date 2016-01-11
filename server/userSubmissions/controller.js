f567f907e0cf4f7a64482acb5b7e11f28f348879
Youngjun Kwak <yk323@georgetown.edu>
2016-01-10 17:26:55 -0800

    Implement Send Report feature

--- server/userSubmissions/controller.js
+++ server/userSubmissions/controller.js
@@ -8,23 +8,43 @@ var findUserInput = Q.nbind(UserSubmission.findOne, UserSubmission);
 
 module.exports = {
 
-  generateReport: function(req, res, next) {
+sendReport: function(req, res, next) {
 
     var username = req.body.username;
-
+    console.log('Generating report for ', username);
+    
     findUserInput({ username: username }).
+      //expect the data to be an object with the latest user entry
       then(function(data){
+        console.log('query result is...', data);
         if (!data) {
           next(new Error('No past submission found from this user'));
         } else {
-          console.log(data);
-          res.json(data);
+          var userData = data;
+          var averageData = util.getAverages();
+          console.log('averageData is...', averageData);
+
+          //report tuple index 0 is the user data and index 1 is the average
+          var report = {
+            income: [userData.income, averageData.income],
+            rent: [userData.rent, averageData.rent],
+            transportation: [userData.transportation, averageData.transportation],
+            restaurants: [userData.restaurants, averageData.restaurants],
+            groceries: [userData.groceries, averageData.groceries],
+            clothes: [userData.clothes, averageData.clothes],
+            hygiene: [userData.hygiene, averageData.hygiene],
+            travel: [userData.travel, averageData.travel],
+            entertainment: [userData.entertainment, averageData.entertainment],
+            gym: [userData.gym, averageData.gym]
+          };
+
+          res.status(200).send(report);
         }
       }).
       fail(function (error) {
-        next(error);
+        res.send(500);
       })
-  },
+  }
 
     // post info acquired from front in (in form of JSON)
   addData: function (req, res, next) {
