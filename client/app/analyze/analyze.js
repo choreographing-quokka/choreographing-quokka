90121395c7d1f57ff238d28604d137eddadeafbe
jotakwak <ykwak323@gmail.com>
2016-01-11 14:00:08 -0800

    Implement Spending Cut recommendation feature

--- client/app/analyze/analyze.js
+++ client/app/analyze/analyze.js
@@ -8,9 +8,13 @@ angular.module('app.analyze', [])
     Results.getResults()
       .then(function(resp) {
       	var results = resp.data;
+        $scope.spendingCutReco = [];
+        console.log(results);
         for (var key in results) {
           results[key].push(results[key][0] - results[key][1] >= 0 ? 'more' : 'less');
-          console.log(results);
+          var recommendation = $scope.recoStrength(results[key][2], $scope.percentDifference(results[key]) , key);
+          console.log(recommendation);
+          $scope.spendingCutReco.push(recommendation);
         }
       	$scope.income = results.income;
         delete results.income;
@@ -33,25 +37,65 @@ angular.module('app.analyze', [])
 
   };
 
-  $scope.recoStrength = function(){
+  $scope.recoStrength = function(spendingMore, percentage, expenditureType){
+    //convert stringified less sign to a minus sign that operates on numbers
+    if(spendingMore === 'less') {
+      percentage = -percentage;
+    }
     var strength;
     var recoStrengthOptions = {
-      veryStrong: 'really should',
-      strong: 'should',
-      moderate: 'may consider',
-      weak: 'could still'
+      4: 'must',
+      3: 'really should',
+      2: 'should',
+      1: 'may consider',
+      0: 'could still'
     };
-    if($scope.consumptionBehaviorScore === 'A+' || 'A'){
-      strength = 'weak';
+
+    //Default recommendating strength
+    if($scope.consumptionBehaviorScore === 'A+'){
+      strength = -2;
+    } else if ($scope.consumptionBehaviorScore === 'A') {
+      strength = -1;
     } else if($scope.consumptionBehaviorScore === 'B+' || 'B'){
-      strength = 'moderate';
+      strength = 0;
     } else if($scope.consumptionBehaviorScore === 'C+' || 'C'){
-      strength = 'strong';
+      strength = 1;
     } else {
-      strength = 'veryStrong';
+      strength = 2;
     }
 
-    return recoStrength[strength];
+    //Luxury factor
+    var luxuryFactor = {
+      clothes: 1 ,
+      eatingout: 1,
+      entertainment: 2,
+      groceries: 0,
+      gym: 0,
+      hygiene: 0,
+      rent: -1,
+      transportation: -1,
+      travel: 1
+    };
+
+    var seriousnessFactor = function(percentage) {
+      if(percentage <= -20) {
+        return -2;
+      } else if (percentage <= 0) {
+        return -1;
+      } else if (percentage < 15) {
+        return 0;
+      } else if (percentage < 30) {
+        return 1;
+      } else {
+        return 2;
+      }
+    };
+
+    strength = strength + luxuryFactor[expenditureType] + seriousnessFactor(percentage);
+
+    // strength cannot exceed the pre-defined maximum
+    if(strength > 4) { strength = 4}
+    return {type: expenditureType, strength: recoStrengthOptions[strength]};
   };
 
   loadResults();
